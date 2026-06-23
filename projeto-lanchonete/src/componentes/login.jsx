bb01833e9ed3aca2603bf008d3cac9c712118541
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-06-23 17:15:22 -0300

    projeto-lanchonete terceiro commit

--- projeto-lanchonete/src/componentes/login.jsx
+++ projeto-lanchonete/src/componentes/login.jsx
@@ -1,9 +1,17 @@
+import './login.css'
+
 function Login(){
   return (
     <>
-    
+    <div className="container">
+
     <h2>Login</h2>
-    
+
+    <input type="text" placeholder=''></input>
+    <input type="password"></input>
+    <button>Enviar</button>
+
+    </div>
     </>
   )
 }
