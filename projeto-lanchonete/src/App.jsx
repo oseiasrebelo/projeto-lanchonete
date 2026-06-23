9636e90c26457cd40e652aeb0877d5b90fd46cc5
oseiasrebelo <oseias.rebelo07@gmail.com>
2026-06-23 14:22:58 -0300

    projeto-lanchonete primeiro commit

---
+++ projeto-lanchonete/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./componentes/Header";
+import Login from "./componentes/login"
+
+function App(){
+  return (
+    <>
+    <Header />
+    <Login />
+    </>
+  )
+}
+
+export default App
---
+++ projeto-lanchonete/src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+<>      
+        <h1>Lanchonete</h1>
+        <h2>O melhor da região</h2>
+</>        
+    )
+}
+
+export default Header
